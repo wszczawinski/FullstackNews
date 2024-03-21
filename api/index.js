8a0f035ab332dca823f71f41ffe0cd1442434b7a
Wojtek Szczawinski <szczawinski.wojtek@gmail.com>
2024-03-21 19:50:04 +0100

    feat(mysql, posts, cors): add mysql connection, get/create post

--- api/index.js
+++ api/index.js
@@ -5,12 +5,39 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const mysql_1 = __importDefault(require("mysql"));
+const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
-const app = (0, express_1.default)();
-const port = process.env.PORT || 3000;
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+const { PORT, MY_SQL_DB_HOST, MY_SQL_DB_USER, MY_SQL_DB_PASSWORD, MY_SQL_DB_DATABASE, } = process.env;
+const db = mysql_1.default.createConnection({
+    host: MY_SQL_DB_HOST,
+    user: MY_SQL_DB_USER,
+    password: MY_SQL_DB_PASSWORD,
+    database: MY_SQL_DB_DATABASE,
 });
-app.listen(port, () => {
-    console.log(`🚀[server]: App  running at http://localhost:${port}`);
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+app.get("/", (req, res) => {
+    res.send("Hello");
+});
+app.get("/posts", (req, res) => {
+    const query = "SELECT * FROM posts";
+    db.query(query, (err, data) => {
+        if (err)
+            res.json(err);
+        return res.json(data);
+    });
+});
+app.post("/posts", (req, res) => {
+    const q = "INSERT INTO posts (`title`, `desc`, `cover`) VALUES (?)";
+    const values = [req.body.title, req.body.desc, req.body.cover];
+    db.query(q, [values], (err, data) => {
+        if (err)
+            res.json(err);
+        return res.json(data);
+    });
+});
+app.listen(PORT, () => {
+    console.log(`🚀[server]: App running at http://localhost:${PORT}`);
 });
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,15 +1,51 @@
 import express from "express";
 import dotenv from "dotenv";
+import mysql from "mysql";
+import cors from "cors";
 
 dotenv.config();
 
-const app = express();
-const port = process.env.PORT || 3000;
+const {
+  PORT,
+  MY_SQL_DB_HOST,
+  MY_SQL_DB_USER,
+  MY_SQL_DB_PASSWORD,
+  MY_SQL_DB_DATABASE,
+} = process.env;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const db = mysql.createConnection({
+  host: MY_SQL_DB_HOST,
+  user: MY_SQL_DB_USER,
+  password: MY_SQL_DB_PASSWORD,
+  database: MY_SQL_DB_DATABASE,
 });
 
-app.listen(port, () => {
-  console.log(`🚀[server]: App  running at http://localhost:${port}`);
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.get("/posts", (req, res) => {
+  const query = "SELECT * FROM posts";
+  db.query(query, (err, data) => {
+    if (err) res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post("/posts", (req, res) => {
+  const q = "INSERT INTO posts (`title`, `desc`, `cover`) VALUES (?)";
+  const values = [req.body.title, req.body.desc, req.body.cover];
+
+  db.query(q, [values], (err, data) => {
+    if (err) res.json(err);
+    return res.json(data);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀[server]: App running at http://localhost:${PORT}`);
 });
