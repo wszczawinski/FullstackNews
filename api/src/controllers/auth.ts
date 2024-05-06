52a27e63872e956a33ffddb15de73724e041dee6
Wojtek Szczawinski <szczawinski.wojtek@gmail.com>
2024-05-07 00:23:27 +0200

    feat(api auth): improve auth, resolve cors issues

--- api/src/controllers/auth.ts
+++ api/src/controllers/auth.ts
@@ -1,9 +1,33 @@
 import bcrypt from "bcrypt";
-import { Request, Response } from "express";
-import { RowDataPacket } from "mysql2";
 import jwt from "jsonwebtoken";
+import { RowDataPacket } from "mysql2";
+import { Request, Response } from "express";
 
 import { db } from "../db";
+
+const { JWT_KEY } = process.env;
+
+const cookieKey = "access_token";
+
+export const register = (req: Request, res: Response) => {
+  const q = "SELECT * FROM user WHERE username = ?";
+
+  db.query<RowDataPacket[]>(q, [req.body.username], (err, data) => {
+    if (err) return res.status(500).json(err);
+    if (data.length) return res.status(409).json("User already exists!");
+
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
+    const q = "INSERT INTO user(`username`,`password`) VALUES (?)";
+    const values = [req.body.username, hash];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("User has been created.");
+    });
+  });
+};
 
 export const login = (req: Request, res: Response) => {
   const q = "SELECT * FROM user WHERE username = ?";
@@ -20,15 +44,23 @@ export const login = (req: Request, res: Response) => {
       return res.status(400).json("Wrong username or password.");
     }
 
-    const token = jwt.sign({ id: data[0].id }, "jwtkey");
+    const token = jwt.sign({ id: data[0].id }, String(JWT_KEY), {
+      expiresIn: "1h",
+    });
 
     res
-      .cookie("access_token", token, { httpOnly: true })
+      .cookie(cookieKey, token, { httpOnly: true })
       .status(200)
-      .json(otherUserData);
+      .json({ ...otherUserData, message: "User logged in successfully!" });
   });
 };
 
 export const logout = (req: Request, res: Response) => {
-  res.json("from auth controller");
+  res
+    .clearCookie(cookieKey, {
+      sameSite: true,
+      secure: true,
+    })
+    .status(200)
+    .json({ message: "User logged out." });
 };
--- api/src/routes/auth.ts
+++ api/src/routes/auth.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { login, logout } from "../controllers/auth";
+import { login, logout, register } from "../controllers/auth";
 
 export const authRoutes = Router();
 
 authRoutes.post("/login", login);
 authRoutes.post("/logout", logout);
+authRoutes.post("/register", register);
